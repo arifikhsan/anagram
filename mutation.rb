e92d48078eb3ababe79d8f8b7b4d8492e2100605
arifikhsan <arif.ikhsanudin.developer@gmail.com>
2019-09-13 15:10:43 +0700

    cleanup

--- mutation.rb
+++ mutation.rb
@@ -1,35 +1,35 @@
 $main_word = 'mahru'
 main_word = $main_word.dup
 $similar = {
-    a: 'e',
-    u: 'n',
-    m: 'w',
-    n: 'u'
+  a: 'e',
+  u: 'n',
+  m: 'w',
+  n: 'u'
 }
 $letters = []
 
 main_word.split('').each_with_index do |letter, index_letter|
-    similiar_keys = $similar.keys.map(&:to_s)
-    if similiar_keys.include? letter
-        $letters << {
-            index: index_letter, 
-            letter_key: letter,
-            letter_mask: $similar[letter.to_sym]
-        }
-    end
+  similiar_keys = $similar.keys.map(&:to_s)
+  next unless similiar_keys.include? letter
+
+  $letters << {
+    index: index_letter,
+    letter_key: letter,
+    letter_mask: $similar[letter.to_sym]
+  }
 end
 
 $data = []
 possibilities = [true, false].repeated_permutation($letters.size).to_a
 
 possibilities.each do |possibility|
-    new_word = $main_word.dup
-    possibility.zip($letters).each do |is_mask, letter|
-        if is_mask
-            new_word[letter[:index]] = letter[:letter_mask]
-            $data << new_word
-        end
+  new_word = $main_word.dup
+  possibility.zip($letters).each do |is_mask, letter|
+    if is_mask
+      new_word[letter[:index]] = letter[:letter_mask]
+      $data << new_word
     end
+  end
 end
 
 # puts $letters
