6a6667d7e1f1cfe3c565ca3c61dda60384a9b5a0
arifikhsan <arif.ikhsanudin.developer@gmail.com>
2019-09-14 23:26:48 +0700

    remove duplicates

--- mutation.rb
+++ mutation.rb
@@ -1,40 +1,37 @@
-$main_word = 'mahru'
-main_word = $main_word.dup
-$similar = {
+main_word = 'mahru'
+similar = {
   a: 'e',
   u: 'n',
   m: 'w',
   n: 'u'
 }
-$letters = []
+letters = []
 
 main_word.split('').each_with_index do |letter, index_letter|
-  similiar_keys = $similar.keys.map(&:to_s)
+  similiar_keys = similar.keys.map(&:to_s)
   next unless similiar_keys.include? letter
 
-  $letters << {
+  letters << {
     index: index_letter,
     letter_key: letter,
-    letter_mask: $similar[letter.to_sym]
+    letter_mask: similar[letter.to_sym]
   }
 end
 
-$data = []
-possibilities = [true, false].repeated_permutation($letters.size).to_a
+data = []
+possibilities = [true, false].repeated_permutation(letters.size).to_a
 
 possibilities.each do |possibility|
-  new_word = $main_word.dup
-  possibility.zip($letters).each do |is_mask, letter|
-    if is_mask
-      new_word[letter[:index]] = letter[:letter_mask]
-      $data << new_word
-    end
+  new_word = main_word.dup
+  possibility.zip(letters).each do |is_mask, letter|
+    new_word[letter[:index]] = letter[:letter_mask] if is_mask
   end
+  data << new_word
 end
 
 # puts $letters
-puts $data.uniq!
-puts $data.size
+puts data
+puts data.size
 
 # [].repeated_permutation
 # [].repeated_combination
